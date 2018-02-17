6e0f74d6bbf4b6f0482e8a9930949688cbcfbec5
j-hannes <hannes_e@gmx.de>
2018-02-17 19:29:02 +0100

    4 - Add todo

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const state = {
     { id: 1, description: 'buy some milk', completed: true },
     { id: 2, description: 'empty bins', completed: false },
   ],
+  nextId: 3,
+  todoInput: '',
 };
 
 const actions = {
@@ -16,6 +18,15 @@ const actions = {
       todo => (todo.id === id ? { ...todo, completed: !todo.completed } : todo)
     ),
   }),
+  updateNewTodo: todoInput => ({ todoInput }),
+  addTodo: () => state => ({
+    todos: [
+      { id: state.nextId, description: state.todoInput, completed: false },
+      ...state.todos,
+    ],
+    nextId: state.nextId + 1,
+    todoInput: '',
+  }),
 };
 
 const view = (state, actions) => (
@@ -23,7 +34,13 @@ const view = (state, actions) => (
     <div>
       <header class="header">
         <h1>todos</h1>
-        <input class="new-todo" placeholder="What needs to be done?" value="" />
+        <input
+          class="new-todo"
+          placeholder="What needs to be done?"
+          value={state.todoInput}
+          oninput={e => actions.updateNewTodo(e.target.value)}
+          onkeyup={e => (e.which === 13 ? actions.addTodo() : null)}
+        />
       </header>
       <section class="main">
         <input class="toggle-all" />
